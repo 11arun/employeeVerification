316b3d693f7412f0e6e6c248f06f8a3b2e1311f2
arun1 <arun1@5400-TN81331>
2024-01-01 19:06:39 +0530

    certification changes

---
+++ EmployeeVerificationSystem/Interface/ICertification.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeVerificationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeVerificationSystem.Interface
+{
+    public interface ICertification
+    {
+        string AddCertification(Certification cn);
+        List<Certification> GetCertificationByEmployeeId(int eid);
+        bool DeleteCertifaction(int cid);
+        Task<int> UpdateCertification(Certification certification);
+    }
+}
